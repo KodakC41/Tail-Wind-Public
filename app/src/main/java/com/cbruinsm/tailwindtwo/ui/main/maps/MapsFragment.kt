fdc1a43bcf774a0ba752d8e40b332580b3fcceea
Christopher Bruisma <63408252+KodakC41@users.noreply.github.com>
2022-05-04 18:36:53 -0400

    Update MapsFragment.kt

--- app/src/main/java/com/cbruinsm/tailwindtwo/ui/main/maps/MapsFragment.kt
+++ app/src/main/java/com/cbruinsm/tailwindtwo/ui/main/maps/MapsFragment.kt
@@ -19,16 +19,21 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
+import com.cbruinsm.tailwindtwo.API.PLANE.QUERY.Track.fixes
+import com.cbruinsm.tailwindtwo.API.PLANE.QUERY.Tracker.positions
 import com.cbruinsm.tailwindtwo.R
 import com.cbruinsm.tailwindtwo.TailWindApp.Companion.CURRENT_LOCATION
 import com.cbruinsm.tailwindtwo.TailWindApp.Companion.NIGHT_MODE
 import com.cbruinsm.tailwindtwo.TailWindApp.Companion.PATH_POINTS
 import com.cbruinsm.tailwindtwo.TailWindApp.Companion.YOUR_NAME
 import com.cbruinsm.tailwindtwo.ui.main.plane.ViewModels.trackViewModel
+import com.cbruinsm.tailwindtwo.ui.main.plane.ViewModels.trackerViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
@@ -42,16 +47,21 @@ import kotlin.properties.Delegates
  */
 class MapsFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var fusedLocationClient: FusedLocationProviderClient //For location.
+    /**
+     * Flight Route
+     */
     private lateinit var ROUTE : trackViewModel
+    /**
+     * Flight Tracker
+     */
+    private lateinit var TRACK : trackerViewModel
     private var longitude = 0.0
     private var latitude = 0.0
     private var name by Delegates.notNull<String>()
     private lateinit var mapFragment : SupportMapFragment
-
     private val prefs: SharedPreferences by lazy {
         PreferenceManager.getDefaultSharedPreferences(requireContext())
     }
-
     //Map Callback that plots for flight path : important checkpoints.
      private val flightPath = OnMapReadyCallback { googleMap ->
         googleMap.uiSettings.isZoomControlsEnabled = true
@@ -60,76 +70,90 @@ class MapsFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListe
             googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(),R.raw.style_json))
         }
         ROUTE.route.observe(viewLifecycleOwner) {
-            // it is the route from the trackViewModel.
-            val routes = ArrayList<LatLng>()
-            if(prefs.getBoolean(PATH_POINTS,true)) {
-                for(i in 0 until it.size) {
-                    latitude = it[i].latitude
-                    longitude = it[i].longitude
-                    name = it[i].name
-                    val type = it[i].type
-                    name = StringBuilder("$name $type").toString()
-                    /**
-                     * Change the color of the destination airport's tag.
-                     */
-                    when (i) {
-                        //Set Destination Marker to Green
-                        it.size-1 -> {
-                            googleMap.addMarker(MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-                                .position(LatLng(latitude, longitude)).title(name)
-                            )
-                        }
-                        else -> {
-                            googleMap.addMarker(MarkerOptions()
-                                .position(LatLng(latitude, longitude)).title(name)
-                            )
-                        }
-                    }
+            Router(it, googleMap)
+        }
 
-                }
-            }
-            else {
-                googleMap.clear()
-                for(i in 0 until it.size) {
-                    routes.add(LatLng(it[i].latitude, it[i].longitude))
-                }
-                var type = it[0].type
-                name = it[0].name
-                name = StringBuilder("$name $type").toString()
-                googleMap.addMarker(MarkerOptions()
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                    .position(LatLng(routes[0].latitude,routes[0].longitude))
-                    .title(name))
-                type = it[routes.size-1].type
-                name = it[routes.size-1].name
-                name = StringBuilder("$name $type").toString()
-                googleMap.addMarker(MarkerOptions()
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-                    .title(name)
-                    .position(LatLng(routes[routes.size-1].latitude,routes[routes.size-1].longitude)))
-                googleMap.addPolyline(PolylineOptions()
-                    .addAll(routes)
-                    .geodesic(true)
-                    .color(R.color.Peach))
-            }
+        TRACK.flightX.observe(viewLifecycleOwner) {
+            Tracker(it,googleMap)
         }
     }
+    private fun Router(it : ArrayList<fixes>, googleMap : GoogleMap) {
+        val routes = ArrayList<LatLng>()
+        if(prefs.getBoolean(PATH_POINTS,true)) {
+            for(i in 0 until it.size) {
+                latitude = it[i].latitude
+                longitude = it[i].longitude
+                name = it[i].name
+                val type = it[i].type
+                name = StringBuilder("$name $type").toString()
+                /**
+                 * Change the color of the destination airport's tag.
+                 */
+                when (i) {
+                    //Set Destination Marker to Green
+                    it.size-1 -> {
+                        googleMap.addMarker(MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                            .position(LatLng(latitude, longitude)).title(name)
+                        )
+                    }
+                    else -> {
+                        googleMap.addMarker(MarkerOptions()
+                            .position(LatLng(latitude, longitude)).title(name)
+                        )
+                    }
+                }
 
+            }
+        }
+        else {
+            googleMap.clear()
+            for(i in 0 until it.size) {
+                routes.add(LatLng(it[i].latitude, it[i].longitude))
+            }
+            var type = it[0].type
+            name = it[0].name
+            name = StringBuilder("$name $type").toString()
+            googleMap.addMarker(MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                .position(LatLng(routes[0].latitude,routes[0].longitude))
+                .title(name))
+            type = it[routes.size-1].type
+            name = it[routes.size-1].name
+            name = StringBuilder("$name $type").toString()
+            googleMap.addMarker(MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                .title(name)
+                .position(LatLng(routes[routes.size-1].latitude,routes[routes.size-1].longitude)))
+            googleMap.addPolyline(PolylineOptions()
+                .addAll(routes)
+                .geodesic(true)
+                .color(R.color.Peach))
+        }
+    }
+    private fun Tracker(it : ArrayList<positions>, googleMap : GoogleMap) {
+        val routes = ArrayList<LatLng>()
+            googleMap.clear()
+            for(i in 0 until it.size) {
+                routes.add(LatLng(it[i].latitude, it[i].longitude))
+            }
+            googleMap.addPolyline(PolylineOptions()
+                .addAll(routes)
+                .geodesic(true)
+                .color(R.color.Peach))
+    }
     //Map Callback that plots for user's current location
     @SuppressLint("MissingPermission")
     private val currentLocation = OnMapReadyCallback { googleMap ->
         googleMap.isMyLocationEnabled = prefs.getBoolean(CURRENT_LOCATION,true)
 
     }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_maps, container, false)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext().applicationContext)
         name = getString(R.string.Go)
         ROUTE = ViewModelProvider(requireActivity()).get(trackViewModel::class.java)
+        TRACK = ViewModelProvider(requireActivity()).get(trackerViewModel::class.java)
         mapFragment = (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)!!
         mapFragment.getMapAsync(flightPath)
         val Checker : LocationManager = context?.getSystemService(LOCATION_SERVICE) as LocationManager
@@ -143,7 +167,6 @@ class MapsFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListe
         }
         return view
     }
-
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -152,10 +175,8 @@ class MapsFragment : Fragment(), SharedPreferences.OnSharedPreferenceChangeListe
     fun settings() {
         startActivity(Intent(Settings.ACTION_SECURITY_SETTINGS))
     }
-
-
     //Get's the phone's location ENSURE APP LOCATION SERVICES ON
-    public fun current() {
+    fun current() {
         if (ActivityCompat.checkSelfPermission(
                 requireContext().applicationContext,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -188,14 +209,12 @@ fun Permissions() {
             show()
         }
     }
-
     /**
      * Unused...
      */
     override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
         TODO("Not yet implemented")
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
     }
